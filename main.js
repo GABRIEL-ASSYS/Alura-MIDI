91de95dd0d5adf2ed75fbc916dd3c0f99dc4bb49
Gabriel Assys <brachak2001@gmail.com>
2022-10-26 18:09:01 -0300

    Segunda Versão

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+    const idAudio = `#som_${instrumento}`;//template String
 
-while(contador < 9) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
 
-    contador++;
-    console.log(contador);
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyuo = function () {
+        tecla.classList.remove('ativa');
+    }
 }
